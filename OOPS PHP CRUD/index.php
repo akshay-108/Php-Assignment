b0ed1fa147f49087142775e51da8f87ff2f8cb94
root <root@DESKTOP-5V1GL77.localdomain>
2021-09-28 01:37:59 +0530

    added pagination in table

--- OOPS PHP CRUD/index.php
+++ OOPS PHP CRUD/index.php
@@ -25,6 +25,24 @@ if(isset($_POST['insert']))
 <title>PHP CRUD</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+<style type="text/css">
+     .pagination {   
+        display: inline-block;   
+    }   
+    .pagination a {   
+        font-weight:bold;   
+        font-size:18px;   
+        color: black;   
+        padding: 8px 16px;   
+        text-decoration: none;   
+        border:1px solid black;   
+    }   
+    .pagination a.active:hover {   
+            background-color: #000;
+            color: #fff;
+        }   
+</style>
 </head>
 <body>
 <div class="container mt-3">
@@ -57,23 +75,36 @@ if(isset($_POST['insert']))
     </thead>
     <tbody>
      <?php
-     $fetchdata=new OOPCRUD();
-      $sql=$fetchdata->fetchData();
-      $cnt=1;
+     $records_per_page=3;
+     $page="";
+      $sql=$ClassObj->fetchData($records_per_page,$page);
       while($row=mysqli_fetch_array($sql))
       {
       ?>
         <tr>
-        <td><?php echo htmlentities($cnt);?></td>
-        <td><?php echo htmlentities($row['name']);?></td>
-        <td><?php echo htmlentities($row['email']);?></td>
-        <td><a class="btn btn-secondary" href="update.php?id=<?php echo htmlentities($row['id']);?>">Edit</a></td>
-        <td><a class="btn btn-danger" href="delete.php?del=<?php echo htmlentities($row['id']);?>">DELETE</a></td>
+            <td><?php echo htmlentities($row['id']);?></td>
+            <td><?php echo htmlentities($row['name']);?></td>
+            <td><?php echo htmlentities($row['email']);?></td>
+            <td><a class="btn btn-secondary" href="update.php?id=<?php echo htmlentities($row['id']);?>">Edit</a></td>
+            <td><a class="btn btn-danger" href="delete.php?del=<?php echo htmlentities($row['id']);?>">DELETE</a></td>
+        </tr>
     <?php
-    // for serial number increment
-    $cnt++;
-    } ?>
+    } 
+    ?>
 </tbody>
 </table>
+<div class="container text-center pagination">
+    <?php
+        $pageLink="";
+        $res=$ClassObj->getPage();
+        $total_pages=ceil($res/$records_per_page);
+        for ($i=1; $i <= $total_pages; $i++) { 
+           
+                echo '<a class="active" href = "index.php?page=' . $i . '">' . $i . ' </a>';
+            } 
+
+    ?>
+</div>
+<br><br><br><br><br><br>
 </body>
 </html>
